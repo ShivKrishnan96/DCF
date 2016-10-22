8d25a1eed394c7b6ef95012c2edfc5e85a558396
ShivKrishnan96 <skrish23@jhu.edu>
2016-10-22 14:58:08 -0400

    third commit

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,9 @@ import requests
 import sys
 import argparse
 from xlrd import open_workbook, XLRDError
+from excel import createExcel
+from DCF import Make_DCF
+
 
 def getFormNumber(s):
     l = s.split('-')
@@ -12,49 +15,54 @@ def getFormNumber(s):
         l[2] = l[2].partition('.')[0]
     return (l[0]+l[1]+l[2],l[1])
 
-def test_book(filename):
-    try:
-        open_workbook(filename)
-    except XLRDError:
-        return False
-    else:
-        return True
+def preprocesser(company):
 
-#args = get_args()
-company = sys.argv[1]
-comp = company.split('_')
-#print comp                                                                                                                                                                                  
-url1 = 'https://www.sec.gov/cgi-bin/srch-edgar?text=company-name%3D%22international+business+machines+corp%22+AND+form-type%3D%2810-k*%29&first=2011&last=2016'
-url = 'https://www.sec.gov/cgi-bin/srch-edgar?text=company-name%3D%22'+comp[0].lower()
-for i in range(1,len(comp)):
-    url = url+'+'+comp[i].lower()
-url = url + '%22+AND+form-type%3D%2810-k*%29&first=2011&last=2016'
-print url
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
-#print(soup.prettify())
-list1 = []
-Accession_number= []
-year = []
-for link in soup.find_all('a'):
-    if ('/Archives')  in (link.get('href')):
-        v = link.get('href')
-        list1.append(link.get('href'))
-        x,y = getFormNumber(v.split('/')[6])
-        Accession_number.append(x)
-        year.append(y)
+	comp = company.split('_')                                                                                                                                                                                  
+	url = 'https://www.sec.gov/cgi-bin/srch-edgar?text=company-name%3D%22'+comp[0].lower()
+	for i in range(1,len(comp)):
+	    url = url+'+'+comp[i].lower()
+	url = url + '%22+AND+form-type%3D%2810-k*%29&first=2013&last=2016'
+	print url
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, "lxml")
+	list1 = []
+	Accession_number= []
+	year = []
+	for link in soup.find_all('a'):
+	    if ('/Archives')  in (link.get('href')):
+		v = link.get('href')
+		list1.append(link.get('href'))
+		x,y = getFormNumber(v.split('/')[6])
+		Accession_number.append(x)
+		year.append(y)
 
-CIK = list1[0].split('/')[4]
-Accession_number = list(set(Accession_number))
-year = list(set(year))
-year.sort()
+	CIK = list1[0].split('/')[4]
+	Accession_number = list(set(Accession_number))
+	year = list(set(year))
+	year.sort()
+	Accession_number.sort()
 
-Accession_number.sort()
+        file_names = []
 
-for i in Accession_number:
-    dls = 'https://www.sec.gov/Archives/edgar/data/' + CIK + '/' + i + '/Financial_Report.xlsx'
+        ctr = 0
 
-    
+	for i in range(3):
+	    dls = 'https://www.sec.gov/Archives/edgar/data/' + CIK + '/' + Accession_number[len(Accession_number) - i - 1] + '/Financial_Report.xlsx'
+	    if createExcel(dls, company+year[len(year) - i-1]+".xlsx"):
+                ctr += 1
+                file_names.append(company+year[len(year) - i-1]+".xlsx")
+
+        if ctr == 3:
+            Make_DCF(file_names)
+        else:
+            print "Not enough data to proceed"
+
+def main():
+    preprocesser(sys.argv[1])
+
+            
+if __name__ == "__main__":
+    main()    
 
     
 
